60129f143553f7442fc5c305cf4daad1d2cec01a
ypwang0408 <61241133+ypwang0408@users.noreply.github.com>
2024-01-09 15:22:38 +0800

    Update test-mpi.py

--- Final Project/Code/test-mpi.py
+++ Final Project/Code/test-mpi.py
@@ -3,7 +3,7 @@ import time
 
 for p in range(1, 9):
 	for d in [960, 1920, 3840, 5760, 7680, 9600, 11520]:
-		call(["mpirun",  "-np",  str(p), "mpi.out",  str(d), "100"])
+		call(["mpirun",  "-np",  str(p), "./bin/mpi.out",  str(d), "100"])
 		print("P=" + str(p) + "   D=" + str(d) + "\n")
 		print("\n\n\n\n")
 		time.sleep(2)
